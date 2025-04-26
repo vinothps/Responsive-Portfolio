f64018b00da585f7cde51672cebcee7282861f44
vinothps <vinothmanan95@gmail.com>
2025-04-26 18:56:59 +0530

    portfolio section fully developed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxs = document.querySelectorAll('.resume-box');
+
 const portfolioLists = document.querySelectorAll('.portfolio-list');
+const portfolioBoxs = document.querySelectorAll('.portfolio-box');
 
 // resume section when clicking tab list
 resumeLists.forEach((list, idx) => {
@@ -19,6 +21,9 @@ portfolioLists.forEach((list, idx) => {
   list.addEventListener('click', () => {
     document.querySelector('.portfolio-list.active').classList.remove('active');
     list.classList.add('active');
+
+    document.querySelector('.portfolio-box.active').classList.remove('active');
+    portfolioBoxs[idx].classList.add('active');
   });
 });
 
